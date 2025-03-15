80fa630eeadb72424b72aef2a3e9c3a5e3925e6f
Margarita (Rita) Busygina <busygina.rita.ca@gmail.com>
2025-03-15 19:09:07 -0400

    get data from the API, match the events and write them in the json files by event type

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //readline is used to create an interface for reading users input and output data
 const readline = require("readline");
+const fs = require("fs");
 const rl = readline.createInterface(process.stdin, process.stdout);
+const githubEvents = JSON.parse(
+  fs.readFileSync("githubEvents.json")
+).githubEvents;
 
 let username = "";
+let recentEvents;
 rl.question("github-activity: ", (response) => {
   username = response;
   getData();
@@ -17,10 +22,42 @@ async function getData() {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    console.log(json);
+    recentEvents = await response.json();
+    filterActivity();
   } catch (error) {
     console.log(error.message);
     console.log("Make sure the username you entered is valid.");
   }
+}
+
+function filterActivity() {
+  //loop over the array,
+  //for each object in the array, get the type of the event and push that object in the json file (only inside of the event)
+  for (let i = 0; i < recentEvents.length; i++) {
+    matchEvent(recentEvents[i]);
+  }
+}
+
+function matchEvent(event) {
+  for (let i = 0; i < githubEvents.length; i++) {
+    if (
+      githubEvents[i].name === event.type &&
+      (event.name !== "CreateEvent" ||
+        githubEvents[i].ref_type === event.ref_type)
+    ) {
+      githubEvents[i] = {
+        ...githubEvents[i],
+        events: [event],
+      };
+      writeJson();
+    }
+  }
+}
+
+function writeJson() {
+  const jsonFile = {
+    githubEvents: githubEvents,
+  };
+  let data = JSON.stringify(jsonFile);
+  fs.writeFileSync("githubEvents.json", data);
 }
