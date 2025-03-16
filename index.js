084181dfd6c79afa4e663ac49862efee83c36493
Margarita (Rita) Busygina <busygina.rita.ca@gmail.com>
2025-03-16 17:47:43 -0400

    change logic to switch statement

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 //readline is used to create an interface for reading users input and output data
 const readline = require("readline");
-const fs = require("fs");
+// const fs = require("fs");
 const rl = readline.createInterface(process.stdin, process.stdout);
-const githubEvents = JSON.parse(
-  fs.readFileSync("githubEvents.json")
-).githubEvents;
+// const githubEvents = JSON.parse(
+//   fs.readFileSync("githubEvents.json")
+// ).githubEvents;
 
 let username = "";
 let recentEvents;
+let messagesToPrint = [];
 rl.question("github-activity: ", (response) => {
   username = response;
   getData();
@@ -21,7 +22,6 @@ async function getData() {
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-
     recentEvents = await response.json();
     filterActivity();
   } catch (error) {
@@ -34,30 +34,31 @@ function filterActivity() {
   //loop over the array,
   //for each object in the array, get the type of the event and push that object in the json file (only inside of the event)
   for (let i = 0; i < recentEvents.length; i++) {
-    matchEvent(recentEvents[i]);
-  }
-}
-
-function matchEvent(event) {
-  for (let i = 0; i < githubEvents.length; i++) {
-    if (
-      githubEvents[i].name === event.type &&
-      (event.name !== "CreateEvent" ||
-        githubEvents[i].ref_type === event.ref_type)
-    ) {
-      githubEvents[i] = {
-        ...githubEvents[i],
-        events: [event],
-      };
-      writeJson();
+    // matchEvent(recentEvents[i]);
+    switch (recentEvents[i].type) {
+      case "CreateEvent":
+        printEvent(recentEvents[i]);
+        break;
+      case "IssuesEvent":
+        messagesToPrint.push(
+          `Opened a new issue in ${recentEvents[i].repo.name}`
+        );
+        break;
     }
   }
+  console.log(messagesToPrint);
 }
 
-function writeJson() {
-  const jsonFile = {
-    githubEvents: githubEvents,
-  };
-  let data = JSON.stringify(jsonFile);
-  fs.writeFileSync("githubEvents.json", data);
+function printEvent(event) {
+  if (event.payload.ref_type === "branch") {
+    messagesToPrint.push(
+      `Created branch ${event.payload.master_branch} in ${event.repo.name}`
+    );
+  } else if (event.payload.ref_type === "repository") {
+    messagesToPrint.push(`Created repository ${event.repo.name}`);
+  } else if (event.payload.ref_type === "tag") {
+    messagesToPrint.push(
+      `Created tag ${event.payload.master_branch} in ${event.repo.name}`
+    );
+  }
 }
