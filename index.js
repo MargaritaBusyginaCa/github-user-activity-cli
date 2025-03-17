73ba9320865b91eadeb327df812c12e4236d4aed
Margarita (Rita) Busygina <busygina.rita.ca@gmail.com>
2025-03-17 18:47:40 -0400

    add pushEvents map and fork event

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const rl = readline.createInterface(process.stdin, process.stdout);
 let username = "";
 let recentEvents;
 let messagesToPrint = [];
+let pushEvents = [];
 rl.question("github-activity: ", (response) => {
   username = response;
   getData();
@@ -19,6 +20,11 @@ async function getData() {
       throw new Error(`Response status: ${response.status}`);
     }
     recentEvents = await response.json();
+    if (recentEvents.length < 1) {
+      console.log(
+        "This user's activity was not found. This might happen because user's GitHub settings disallow public events"
+      );
+    }
     filterActivity();
   } catch (error) {
     console.log(error.message);
@@ -44,10 +50,26 @@ function filterActivity() {
         messagesToPrint.push(`Starred ${recentEvents[i].repo.name}`);
         break;
       case "PushEvent":
-        handlePushEvent(recentEvents[i]);
+        pushEvents.push(recentEvents[i]);
         break;
+      case "PublicEvent":
+        messagesToPrint.push(`Made ${recentEvents[i]?.repo?.name} public`);
+        break;
+      case "PullRequestEvent":
+        messagesToPrint.push(
+          `${recentEvents[i]?.action} a pull request "${recentEvents[i]?.pull_request?.title}"`
+        );
+        break;
+      case "ForkEvent":
+        messagesToPrint.push(`Forked ${recentEvents[i]?.repo?.name}`);
+        break;
+      default:
+        messagesToPrint.push(
+          `${recentEvents[i]?.type} at ${recentEvents[i]?.repo?.name}`
+        );
     }
   }
+  handlePushEvent();
   messagesToPrint.forEach((message) => {
     console.log(`- ${message}`);
   });
@@ -67,17 +89,19 @@ function printEvent(event, eventType) {
   }
 }
 
-let pushEvents = [];
-let groupedPushEvents = [];
-function handlePushEvent(event) {
-  for (let i = 0; i < recentEvents.length; i++) {
-    if (recentEvents[i].type === "PushEvent") {
-      pushEvents.push(recentEvents[i]);
+let pushEventsMap = {};
+function handlePushEvent() {
+  pushEvents.forEach((event) => {
+    if (pushEventsMap[event?.repo?.name] === undefined) {
+      pushEventsMap[event?.repo?.name] = 1;
+    } else {
+      pushEventsMap[event?.repo?.name]++;
     }
-  }
-  for (let i = 0; i < recentEvents.length; i++) {
-    groupedPushEvents = pushEvents.find(
-      (element) => element.repo.name === event.repo.name
+  });
+
+  for (let pushRepo in pushEventsMap) {
+    messagesToPrint.push(
+      `Pushed ${pushEventsMap[pushRepo]} commits to ${pushRepo}`
     );
   }
 }
